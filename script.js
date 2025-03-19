2756daaef3db23b109cbc4c8b0acf1bb4600d385
Tom <hello@tsaunders.dev>
2025-03-19 10:08:37 +0000

    Gameboard and game controller setup

---
+++ script.js
@@ -0,0 +1,38 @@
+
+/* Gameboard */
+
+const GameBoard = (function () {
+    const board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
+});
+
+
+const gameController {
+
+  playerOne = {
+    name: playerOneName, 
+    score: 0
+  },
+
+  playerTwo = {
+    name: playerTwoName,
+    score: 0
+  }
+
+}
+
+
+/* */
+
+/* Player 1 + 2 */ 
+
+/* Gameflow */ 
+
+/* Score */
+
+/*Win / lose */ 
+
+
