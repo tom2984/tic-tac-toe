368696f3a9690277332ba0a040480b177408e815
Tom <hello@tsaunders.dev>
2025-03-19 10:12:32 +0000

    private game state IIFE

--- script.js
+++ script.js
@@ -10,21 +10,19 @@ const GameBoard = (function () {
 });
 
 
-const gameController = {
+const gameController = (function() {
+  // Private game state
+  const playerOne = {
+      name: "Player One",
+      score: 0
+  };
 
-  playerOne: {
-    name: playerOneName, 
-    score: 0
-  },
+  const playerTwo = {
+      name: "Player Two",
+      score: 0
+  };
 
-  playerTwo: {
-    name: playerTwoName,
-    score: 0
-  }
-
-};
-
-
+});
 
 
 /* */
